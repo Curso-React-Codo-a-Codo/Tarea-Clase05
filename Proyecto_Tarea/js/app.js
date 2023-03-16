b912b523d5e63ae4ccaaafc8f8c91935db7d4a99
Javier Gil <javiergonzalog22@gmail.com>
2023-03-16 18:06:17 -0300

    Se crea el input usando array en JS

---
+++ Proyecto_Tarea/js/app.js
@@ -0,0 +1,52 @@
+let MyDom = document
+let MyApp = document.getElementById("MyApp")
+class Button {
+    constructor(parentID, text, id){
+        this.parentID = parentID;// el id del div en el cual estara el boton
+        this.text = text; //Texto que le puedo pasar
+        this.id = id; //paremetro que le puedo pasar
+    }
+    render(){
+        let myContenedor = document.getElementById(this.parentID)
+        const myButtonHTML = "<button id="+this.id+">"+this.text+"</button>"//test
+
+        myContenedor.innerHTML = myButtonHTML; //Aca se va a generar el html del boton
+    }
+}
+
+let myButton = new Button("contenedor","Presiona aquí!","boton");
+myButton.render();
+const boton = document.getElementById('boton');
+boton.addEventListener('click', function() {
+	const nombre = prompt('¿Cuál es tu nombre?');
+    if (nombre == null || nombre == "" )
+    {
+        MyApp.innerHTML = "Dejaste el Promt Vacio o lo Cancelaste";
+    } else {
+        MyApp.innerHTML = "Hola! "+ nombre;
+    }
+	    
+});
+
+
+class Input {
+    constructor(parentID, type) {
+        this.parentID = parentID;
+        this.type = type;
+    }
+    render(){
+        let input = document.getElementById(this.parentID);
+        const myInputHTML = input.innerHTML + "<input type="+this.type+">";
+        input.innerHTML = myInputHTML;
+    }
+}
+
+let Array_of_inputs = ["checkbox","date","password"];
+
+function CrearInpunt(input){
+    let myInput = new Input("fecha",input);
+    myInput.render();
+}
+
+//array_filter = Array_of_inputs.filter(Array_of_inputs => Array_of_inputs.length < 5 )
+Array_of_inputs.forEach(element => CrearInpunt(element));
